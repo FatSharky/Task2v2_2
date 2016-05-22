de7f5ad84d286ae745f87fbbafe8e29a1f9be275
Sharkai <vladislavbypinsk@mail.ru>
2016-05-22 23:15:29 +0300

    Implement document

---
+++ DomParse/src/by/training/gapeenko/task2/implementation/DocumentImpl.java
@@ -0,0 +1,53 @@
+package by.training.gapeenko.task2.implementation;
+
+import org.w3c.dom.DOMException;
+
+import by.training.gapeenko.task2.inteface.Document;
+import by.training.gapeenko.task2.inteface.Element;
+import by.training.gapeenko.task2.inteface.Node;
+
+public class DocumentImpl implements Document{
+	private Element documentElement;
+	@Override
+	public short getNodeType() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getNodeValue() throws DOMException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Node getParentNode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Node getFirstChild() {
+		// TODO Auto-generated method stub
+		return documentElement;
+	}
+
+	@Override
+	public Node getLastChild() {
+		// TODO Auto-generated method stub
+		return documentElement;
+	}
+
+	@Override
+	public Element createElement(String tagName) throws DOMException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Element getDocumentElement() {
+		// TODO Auto-generated method stub
+		return documentElement;
+	}
+
+}
