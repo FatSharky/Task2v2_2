3e81396025cb9922c98db73faf7ba218d9c6bd0c
Sharkai <vladislavbypinsk@mail.ru>
2016-05-22 23:40:28 +0300

    Implements Text

--- DomParse/src/by/training/gapeenko/task2/implementation/TextImpl.java
+++ DomParse/src/by/training/gapeenko/task2/implementation/TextImpl.java
@@ -1,5 +1,68 @@
 package by.training.gapeenko.task2.implementation;
 
-public class TextImpl {
+import org.w3c.dom.DOMException;
 
+import by.training.gapeenko.task2.inteface.Node;
+import by.training.gapeenko.task2.inteface.Text;
+
+public class TextImpl implements Text {
+	private String value;
+
+
+	@Override
+	public short getNodeType() {
+		// TODO Auto-generated method stub
+		return 3;
+	}
+
+	@Override
+	public String getNodeValue() throws DOMException {
+		// TODO Auto-generated method stub
+		return value;
+	}
+
+	@Override
+	public Node getParentNode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Node getFirstChild() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Node getLastChild() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getWholeText() {
+		// TODO Auto-generated method stub
+		return value;
+	}
+
+	@Override
+	public void replaceWholeText(String content) {
+
+		value = content;
+
+	}
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		return "TextImpl [value=" + value + "]";
+	}
+
+	
 }
