dadd49858f9d0cedad23216ceb74fe4bc15a5bef
Sharkai <vladislavbypinsk@mail.ru>
2016-05-27 22:36:08 +0300

    Create new interface with variables

---
+++ DomParse/src/by/training/task3/domains/intefrace/Ivariables.java
@@ -0,0 +1,12 @@
+package by.training.task3.domains.intefrace;
+
+public interface Ivariables {
+	char START_TAG = '<';
+	char END_TAG = '>';
+	char CLOSE_TAG = '/';
+	char QUOTES = '"';
+	char SPACE = ' ';
+	char EQUALLY= '=';
+	char QUESTION = '?';
+
+}
